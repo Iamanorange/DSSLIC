a070de0781ba72154fca929778f7528eebdee8b8
Iamanorange <freepascal@vip.qq.com>
2018-08-16 13:09:33 +0800

    set() is a built-in class in Python3. There is no need to import.

--- models/base_model.py
+++ models/base_model.py
@@ -70,8 +70,7 @@ class BaseModel(torch.nn.Module):
                     print('Pretrained network %s has excessive layers; Only loading layers that are used' % network_label)
                 except:
                     print('Pretrained network %s has fewer layers; The following are not initialized:' % network_label)
-                    from sets import Set
-                    not_initialized = Set()
+                    not_initialized = set()
                     for k, v in pretrained_dict.items():                      
                         if v.size() == model_dict[k].size():
                             model_dict[k] = v
